e98c443d14df74286d10be8171e875f312c15725
JunpeiSakai <k.jkmt2y4@ezweb.ne.jp>
2016-12-19 22:35:09 +0900

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,41 @@
+﻿$(loaded);
+
+
+
+function loaded(){
+  showText();
+  $("#formButton").click(
+    function(){
+      saveText();
+      showText();
+    });
+}
+
+function saveText(){
+  var text = $("#formText");
+  var time = new Date();
+  localStorage.setItem(time,text.val());
+  text.val("");
+}
+
+function showText(){
+  var list = $("#list");
+  list.children().remove();
+  var key,value,html = [];
+  for(var i=localStorage.length,len=0;i>=len;i--){    
+      key = localStorage.key(i);
+      value = localStorage.getItem(key);
+      html.push($("<p; display: inline-block>").text(value),"<br>");
+      if(i!=0){
+        html.push($('<input type="checkbox">'));
+     }
+  }
+  list.append(html);
+  
+}
+
+
+
+
+
+
